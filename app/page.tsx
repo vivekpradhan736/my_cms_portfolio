7bf69613454b5ed75bf4e32da449723f64decdb3
Vivek <vivekpradhan736@gmail.com>
2024-02-28 00:17:37 +0530

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
-import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
 
-  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div>
